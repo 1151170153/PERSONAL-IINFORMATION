900f44940f5324df1181ac5bdb455edf339013b2
1151170153 <singhsumit62071@gmail.com>
2025-01-30 12:10:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript can be used for dynamic interactions (for now it's empty)
+// Example: Smooth scrolling when clicking on navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
